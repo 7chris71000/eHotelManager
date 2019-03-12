2366210e51fdd43e48d38f350eb2080ebeb32a9f
Christopher <c.francis7@live.ca>
2019-03-11 22:10:55 -0400

    Setup Filter

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -2,6 +2,31 @@ class HotelsController < ApplicationController
 
 	def index
 		@form_data = params
+		params["capacity"] != "" ? capacity = params["capacity"] : capacity = "'%'"
+		params["chain"] != "" ? chain = params["chain"] : chain = "'%'"
+		params["rating"] != "" ? rating = params["rating"] : rating = "'%'"
+		params["number_of_rooms"] != "" ? noOfRooms = params["number_of_rooms"] : noOfRooms = "'%'"
+		params["price"] != "" ? price = params["price"] : price = "'%'" 
+
+		results = ActiveRecord::Base.connection.execute(
+				"
+				SELECT * 
+				FROM hotels h
+				JOIN hotel_chains hc
+				on h.ChainID = hc.ChainID
+				JOIN rooms
+				on rooms.HotelID = h.HotelID
+				WHERE Capacity LIKE '#{capacity}'
+				AND Name LIKE '#{chain}'
+				AND Category LIKE '#{rating}'
+				AND NoOfRooms LIKE '#{noOfRooms}'
+				AND Price LIKE '#{price}'
+				;
+				"
+			);
+
+		@hotels = results
+	 
 	end
 
 	def new
