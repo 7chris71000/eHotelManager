0ca4ab585cc2ba7e466b6a1ee501e054f5b7cd5a
Christopher <c.francis7@live.ca>
2019-03-20 10:53:44 -0400

    Added radio buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,89 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_004531) do
+ActiveRecord::Schema.define(version: 2019_03_20_141245) do
+
+  create_table "amenities", id: false, force: :cascade do |t|
+    t.integer "RoomID"
+    t.string "Amenity", limit: 255
+  end
+
+  create_table "archive", primary_key: "ArchiveID", force: :cascade do |t|
+    t.integer "RoomID"
+    t.integer "CustomerID"
+    t.decimal "Price", precision: 6, scale: 2
+    t.integer "Capacity"
+    t.string "BookedRented", limit: 255
+    t.date "ArchiveCheckin"
+    t.date "ArchiveCheckout"
+  end
+
+  create_table "customer_archives", id: false, force: :cascade do |t|
+    t.integer "ArchiveID"
+    t.integer "CustomerID"
+  end
+
+  create_table "customer_rooms", id: false, force: :cascade do |t|
+    t.integer "RoomID"
+    t.integer "CustomerID"
+    t.date "Checkin"
+    t.date "Checkout"
+  end
+
+  create_table "customers", primary_key: "CustomerID", force: :cascade do |t|
+    t.string "CFirstName", limit: 255
+    t.string "CLastName", limit: 255
+    t.integer "SSNSIN"
+    t.string "CustomerAddress", limit: 255
+    t.date "DateOfRegistration"
+  end
+
+  create_table "email_addresses", id: false, force: :cascade do |t|
+    t.integer "ChainID"
+    t.string "EmailAddress", limit: 255
+  end
+
+  create_table "employees", primary_key: "EmployeeID", force: :cascade do |t|
+    t.string "EFirstName", limit: 255
+    t.string "ELastName", limit: 255
+    t.string "EmployeeAddress", limit: 255
+    t.integer "SSNSIN"
+    t.integer "HotelID"
+  end
+
+  create_table "hotel_chains", primary_key: "ChainID", force: :cascade do |t|
+    t.string "ChainName", limit: 255
+    t.string "OfficeAddress", limit: 255
+    t.integer "NoOfHotels"
+  end
+
+  create_table "hotels", primary_key: "HotelID", force: :cascade do |t|
+    t.string "HotelAddress", limit: 255
+    t.string "City", limit: 255
+    t.string "ContactEmail", limit: 255
+    t.integer "NoOfRooms"
+    t.integer "Category"
+    t.integer "ChainID"
+  end
+
+  create_table "phone_numbers", id: false, force: :cascade do |t|
+    t.integer "ChainID"
+    t.decimal "PhoneNo", precision: 10
+  end
+
+  create_table "roles", id: false, force: :cascade do |t|
+    t.integer "EmployeeID"
+    t.string "Role", limit: 255
+  end
+
+  create_table "rooms", primary_key: "RoomID", force: :cascade do |t|
+    t.decimal "Price", precision: 6, scale: 2
+    t.integer "Capacity"
+    t.boolean "Extendable"
+    t.string "Status", limit: 255
+    t.string "View", limit: 255
+    t.integer "HotelID"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,10 +100,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_004531) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "customer"
-    t.boolean "employee"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
